dc226cd03ced22fb298d904be06963ab226ddeb3
Rogier van den Berg <rogiervandenberg@gmail.com>
2013-02-25 19:34:18 +0100

    Removed all UDP/Ethernet stuff, things are going over Serial now (and Stable!)

--- arduinobridgerf.ino
+++ arduinobridgerf.ino
@@ -5,17 +5,12 @@
  * Its purpose is to function as a proxy/bridge, so you can toggle lights or receive commands in your domotica solution.
  *
  * It is designed to work with combined with https://github.com/bslagter/hue-control
- * You need an Arduino with Ethternet for this to work.
  * 
  * I am using the interpretation of RF-signals from the RemoteSwitch Libraries by Randy Simons,
  * as found at https://bitbucket.org/fuzzillogic/433mhzforarduino/wiki/Home
  * 
- * For processing the raw RF-signals for the New style KaKu I am using the code from Tymmo at http://gathering.tweakers.net/forum/list_message/37257667#37257667
  *
  ==How to use==
- *  Send an UDP Package to port 8888 to execute (send) a command over RF.
- *  Listen for UDP Packages on port 8889 to receive RF-commands
- * 
  * A command is built with 5 comma separated values:
  *  - Direction of the message: O or I (outgoing, incoming);
  *  - Library that applies: 1, 2 or 3 (NewRemoteSwitch, RemoteSwitch or RemoteSensor)
@@ -31,44 +26,29 @@
 #include <NewRemoteReceiver.h>
 #include <NewRemoteTransmitter.h>
 #include <InterruptChain.h>
-#include <SPI.h>
-#include <Ethernet.h>
 #include <String.h>
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //Mac Address (IP will be dynamic by DHCP)
-IPAddress broadcast(255, 255, 255, 255); //Where to broadcast incoming signals to
-unsigned int localPort = 8888;      // local port to listen on
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
-char outgoingPacketBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold outgoing packet,
+String incomingPacketBuffer = ""; //buffer to hold incoming packet,
+String outgoingPacketBuffer = ""; //buffer to hold outgoing packet,
+char packetBuffer[50]; //buffer to hold incoming packet,
 
 //Connections of the RF transmitter and LED to the Arduino board (receiver is 2)
 const int led =  4; 
 const int transmitterPin = 8;
 
-// An EthernetUDP instance to let us send and receive packets over UDP
-EthernetUDP Udp;
-
 boolean sending = false;
-boolean udpMessageToSend = false;
+boolean serialMessageToSend = false;
+boolean stringComplete = false;  // whether the string is complete
 
 void setup()
 {
   pinMode(led, OUTPUT); 
   
+  incomingPacketBuffer.reserve(50);
+  outgoingPacketBuffer.reserve(50);
+  
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  
-  // start the Ethernet connection:
-  Serial.println("Trying to get an IP address using DHCP");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-  }
-  // print your local IP address:
-  Serial.print("My IP address: ");
-  Serial.println(Ethernet.localIP());
-  Serial.println();
-  
-  Udp.begin(localPort);
   
     // Interrupt -1 to indicate you will call the interrupt handler with InterruptChain
   RemoteReceiver::init(-1, 2, handleOldRfRemotes);
@@ -83,40 +63,33 @@ void setup()
 
 void loop() {
     
-  sendUDPMessage();
-  
-  // if there's data available, read a packet
-  if(Udp.parsePacket()) {
-    sending = true;
-    // read the packet into packetBufffer
-    Udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
-    Serial.print("Received over UDP: ");
-    Serial.println(packetBuffer);
+  if (stringComplete) {
+    //Serial.println(incomingPacketBuffer); 
+    incomingPacketBuffer.toCharArray(packetBuffer, 50);
     handleCommand(packetBuffer);
-    sending = false;
+    // clear the string:
+    incomingPacketBuffer = "";
+    stringComplete = false;
+  }
+  
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read(); 
+    // add it to the inputString:
+    incomingPacketBuffer += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
   }
 }
-
-//This message checks if there is a message to send, if yes, it will be sent
-void sendUDPMessage() {
-  if (udpMessageToSend == true) {
-    Serial.print("We send this message out over UDP: ");
-    Serial.println(outgoingPacketBuffer);
-    sending = true;
-    //Broadcasting RF-signal
-    Udp.beginPacket(broadcast, 8889);
-    Udp.write(outgoingPacketBuffer);
-    Udp.endPacket(); 
-    
-    udpMessageToSend = false;
-    sending = false;
-  }
-}
-
+  
 //If we receive a command, parse it to do something with it
 void handleCommand(char* receivedCommand) {
   char *p = receivedCommand;
-  char *str;
+  
+   char *str;
   int count = 0;
   String commandValues[5];
   
@@ -136,14 +109,19 @@ void handleCommand(char* receivedCommand) {
     
     commandValues[count++] = str;
   }
-  if (commandValues[0] == "O") {
+  
+  if (commandValues[0] == "O" && commandValues[1] == "2") {
+    /*
     Serial.print("Sending out over RF: ");
     Serial.print(receivedCode);
     Serial.print(" / ");
     Serial.println(period);
+    */
     RemoteReceiver::disable();
     RemoteTransmitter::sendCode(8, receivedCode, period, 3);
     RemoteReceiver::enable();
+  } else if (commandValues[0] == "O" && commandValues[1] == "1") {
+    Serial.println("new kakuding");
   }
 }
 
@@ -153,26 +131,24 @@ void handleCommand(char* receivedCommand) {
 void handleOldRfRemotes(unsigned long receivedCode, unsigned int period) {
   
   // Print the received code.
+  /*
   Serial.print("Interupted! Code: ");
   Serial.print(receivedCode);
   Serial.print(", period: ");
   Serial.print(period);
   Serial.println("us.");
-  
+  */
   String codeString(receivedCode);
   String periodString(period);
   
   String command = "I,2," + codeString + "," + periodString + ",0000";
-  if(udpMessageToSend == false) {
-    udpMessageToSend = true;
-    command.toCharArray(outgoingPacketBuffer, 50);
-  }
+  Serial.println(command);
 }
 
 
 // shows the received code sent from an old-style remote switch
 void handleNewRfRemotes(NewRemoteCode receivedCode) {
-  
+  /*
   // Print the received code.
   Serial.print("Addr ");
   Serial.print(receivedCode.address);
@@ -200,7 +176,7 @@ void handleNewRfRemotes(NewRemoteCode receivedCode) {
   Serial.print(", period: ");
   Serial.print(receivedCode.period);
   Serial.println("us.");
-  
+  */
   
   String codeString(receivedCode.address);
   String periodString(receivedCode.period);
@@ -212,26 +188,9 @@ void handleNewRfRemotes(NewRemoteCode receivedCode) {
   }
   
   String command = "I,1," + codeString + "," + periodString + "," + typeString + unitString + "0";
-  if(udpMessageToSend == false) {
-    udpMessageToSend = true;
-    command.toCharArray(outgoingPacketBuffer, 50);
-  }
+  Serial.println(command);
 }
 
-
-void transmitCode(unsigned long receivedCode, unsigned int period) {  
-  
-  Serial.print("We gaan sturen: ");
-  Serial.print(receivedCode);
-  Serial.print(" / ");
-  Serial.println(period);
-
-  
-  RemoteReceiver::disable();
-  interrupts();
-  RemoteTransmitter::sendCode(8, receivedCode, period, 3);
-  RemoteReceiver::enable();
-}
 
 void transmitCodeNewKaKu(NewRemoteCode receivedCode) {  
   NewRemoteReceiver::disable();
